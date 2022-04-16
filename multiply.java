5c768bc214ad02125a840769d7b3af4c0a2715d5
Rahul <rahul@patientbond.com>
2022-04-17 01:41:47 +0530

    added multiply.java

---
+++ multiply.java
@@ -0,0 +1,17 @@
+package Git;
+
+public class multiply {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a=10;
+		int b=20;
+		int c=a*b;
+		System.out.println("The multiply of two numbers is equal to: " +c);
+		
+		
+		
+	}
+
+}
