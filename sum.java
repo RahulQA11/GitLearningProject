e70598299b24a0b59894f9fa80eec3b4100f657c
Rahul <rahul@patientbond.com>
2022-04-16 23:58:50 +0530

    Sum program commit 1

---
+++ sum.java
@@ -0,0 +1,17 @@
+package Git;
+
+public class sum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a=10;
+		int b=20;
+		int c=a+b;
+		System.out.println("The sum of two numbers is: " +c);
+		
+		
+		
+	}
+
+}
